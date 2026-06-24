46c4c466fe2c1472b0983339c079aff45da58104
Andrews Nícolas <pacificonicolas77@gmail.com>
2026-06-23 21:56:35 -0300

    Adição e iniciação da 5° parte do projeto

--- Módulos de Estudo/Linux Básico/Terminal do Linux/Projetos/Auditoria e Logs/auditoria/script.sh
+++ Módulos de Estudo/Linux Básico/Terminal do Linux/Projetos/Auditoria e Logs/auditoria/script.sh
@@ -11,4 +11,9 @@ awk -F ' ' '!seen[$4]++ {print $4}' acessos.log | head -n 3
 
 awk -F ' ' '!seen[$4]++ {print $4}' acessos.log | tail -n 2
 
+awk -F ' ' '{print substr($2, 1, 2)}' acessos.log | sort -k 2 > teste.txt
+
+
+
+
 
